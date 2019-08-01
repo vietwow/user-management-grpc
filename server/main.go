336632ef53ddb536f6c671422639f22a6ef765c0
Viet Nguyen <viet.nguyen@quoine.com>
2019-08-02 02:11:56 +0700

    init commit

--- server/main.go
+++ server/main.go
@@ -19,17 +19,17 @@ const (
 
 type UserService struct {}
 
-// func NewUserService() UserService {
-// 	return &UserService{}	
-// }
+func NewUserService() *UserService {
+	return &UserService{}
+}
 
-// func(s *server) ListUser(ctx context.Context, in *pb.ListUserRequest) (*pb.ListUserResponse, error) {
-//     // for _, user := range s.savedUsers {
-//     //     ...
-//     // }
-//     userId := 1
-//     return &pb.ListUserResponse{Id: userId, Success: true}, nil
-// }
+func(s *UserService) ListUser(ctx context.Context, in *pb.ListUserRequest) (*pb.ListUserResponse, error) {
+    // for _, user := range s.savedUsers {
+    //     ...
+    // }
+    // userId := 1
+    return &pb.ListUserResponse{}, nil
+}
 
 func(s *UserService) CreateUser(ctx context.Context, in *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
     log.Printf("Received: %v", in.User.UserId)
@@ -37,11 +37,11 @@ func(s *UserService) CreateUser(ctx context.Context, in *pb.CreateUserRequest) (
 	return &pb.CreateUserResponse{UserId: in.User.UserId, Success: true}, nil
 }
 
-// func(s *UserService) GetUser(ctx context.Context, in *pb.GetUserRequest) (*pb.GetUserResponse, error) {
-//     log.Printf("Received: %v", in.Username)
+func(s *UserService) GetUser(ctx context.Context, in *pb.GetUserRequest) (*pb.GetUserResponse, error) {
+    log.Printf("Received: %v", in.Username)
 
-// 	return &pb.GetUserResponse{User: {UserId: 1, Username: 'vietwow', Email: 'vietwow@gmail.com', Password: '123456', Pshone: '123456'}}, nil
-// }
+	return &pb.GetUserResponse{}, nil
+}
 
 func(s *UserService) DeleteUser(ctx context.Context, in *pb.DeleteUserRequest) (*pb.DeleteUserResponse, error) {
     log.Printf("Received: %v", in.Username)
@@ -57,7 +57,6 @@ func main() {
 
 	// Creates a new gRPC server
 	s := grpc.NewServer()
-	// service := pb.UserServiceServer(&UserService{})
 	pb.RegisterUserServiceServer(s, &UserService{})
 
 	// graceful shutdown
